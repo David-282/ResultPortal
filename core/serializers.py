cff0ca109330bcdb1e852926da66747c1ff7e335
David-282 <afolabidavid94@gmail.com>
2026-05-25 23:03:07 +0100

    third commit

--- core/serializers.py
+++ core/serializers.py
@@ -6,7 +6,7 @@ from core.models import Department
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
-        fields = ['code', 'name', 'description']
+        fields = ['department_code', 'name', 'description']
     # code = serializers.CharField(max_length=55, required=True)
     # name =  serializers.CharField(max_length=55, required=True)
     # description = serializers.CharField(max_length=255, required=False)
