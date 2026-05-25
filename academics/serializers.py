f5bee28ef5d5cea358a044e93f0eaf575cb831d5
David-282 <afolabidavid94@gmail.com>
2026-05-25 09:04:14 +0100

    another commit

--- academics/serializers.py
+++ academics/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from academics.models import Course
-from core.constants import LEVEL_CHOICES, SEMESTER_CHOICES
 
 
 class CourseSerializer(serializers.Serializer):
