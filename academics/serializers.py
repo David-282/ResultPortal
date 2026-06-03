3125e18a471c0f743d78f7f79bc145c9b853f801
David-282 <afolabidavid94@gmail.com>
2026-06-03 12:36:21 +0100

    fifth commit

--- academics/serializers.py
+++ academics/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from academics.models import Course
+from academics.models import Course, AcademicSession
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -12,3 +12,11 @@ class CourseSerializer(serializers.ModelSerializer):
     def create(self,validated_data):
         department_id = self.context.get('department_id')
         return Course.objects.create(department_id=department_id, **validated_data)
+
+
+class AcademicSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AcademicSession
+        fields = ('name','year','semester','is_current','start_date','end_start')
+
--- academics/urls.py
+++ academics/urls.py
@@ -1,16 +1,15 @@
 #
 #
-# from django.urls import path, include
-# from .views import CourseView
-# from rest_framework import routers
-#
-# router = routers.DefaultRouter()
-# router.register('courses', CourseView, basename='courses')
-#
-#
-#
-#
-# urlpatterns = [
-#     path("", include(router.urls)),
-#
-# ]
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import AcademicViewSet
+
+router = routers.DefaultRouter()
+router.register('academic_session', AcademicViewSet, basename='academic_session')
+
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
