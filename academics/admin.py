d6d30bb368af7683340d4d844752ebcef0ac380a
David-282 <afolabidavid94@gmail.com>
2026-06-09 15:11:45 +0100

    fifth commit

--- academics/admin.py
+++ academics/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from academics.models import Course
+
+
 # Register your models here.
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title','department','credit_unit','code', 'created_at', 'updated_at')
+    search_fields = ('title','code','department','credit_unit')
